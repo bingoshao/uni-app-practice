7ecf73361e17259b6fb36da3f849cacdbc8159ad
smj <shao.mengjie@skyman.cn>
2020-08-06 08:45:27 +0800

    update

---
+++ pages/desktop/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="content">
+		桌面
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+view {
+	color: $color-primary;
+}
+</style>
